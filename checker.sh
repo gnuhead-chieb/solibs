a6aba3f8f9159037917fc5e28621079b51281e50
gnuhead-chieb <41156994+gnuhead-chieb@users.noreply.github.com>
2021-11-24 19:51:08 +0900

    Update checker

--- checker.sh
+++ checker.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+## License:GPLv3
+##Usage:./checker.sh *.apk [OPTIONAL:solibs.json]
+
 APK="$1"
-signatures=solibs.json
+signatures="$2"
+
+[ -z "$2" ] && signatures=solibs.json
+[ -z "$APK" ] && { echo "APK file not specified! exiting..." ; exit 0; }
 
 checks=$(zip -sf -r "$APK" | grep -e '\.so$' | awk -F/ '{print $NF}')
-
 
 for i in `seq 0 $(($(jq -r .[].signature "$signatures" | wc -l)-1))`
 do
