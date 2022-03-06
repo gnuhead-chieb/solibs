b040faf02e9d9c8cd86e80d70559e9647a4fd060
gnuhead-chieb <41156994+gnuhead-chieb@users.noreply.github.com>
2022-03-06 22:48:52 +0900

    Update find_missing.sh

--- find_missing.sh
+++ find_missing.sh
@@ -6,6 +6,27 @@
 ##      2.Run script
 ##        ./find_missing.sh [OPTIONAL:solibs.json]
          
-signatures="$1"
-[ -z "$1" ] && signatures=solibs.json
-echo "pm list package | grep -vo -e \"^package:\" | xargs -n 1 -P 0 pm path | grep -vo -e \"^package:\" | xargs -n 1 zip -sf -r | grep -e '\.so$' | awk -F/ '{print \$NF}'" | adb shell | sort | uniq | grep -vP $(jq -r .[].signature $signatures | sed -z "s/\n/|/g" | sed -e "s/|$//g")
+function all_installed_apps {
+signatures=solibs.json
+echo "pm list package | grep -vo -e \"^package:\" | xargs -n 1 -P 0 pm path | grep -vo -e \"^package:\" | xargs -n 1 unzip -l | grep -e '\.so$' | awk -F/ '{print \$NF}'" | adb shell | sort | uniq | grep -vP $(jq -r .[].signature $signatures | sed -z "s/\n/|/g" | sed -e "s/|$//g")
+}
+
+function apk {
+signatures=solibs.json
+APK="$2"
+[ -z "$2" ] && exit 0
+zip -sf -r "$APK" | grep -e '\.so$' | awk -F/ '{print $NF}' | sort | uniq | grep -vP $(jq -r .[].signature $signatures | sed -z "s/\n/|/g" | sed -e "s/|$//g")
+}
+
+function package_name {
+signatures=solibs.json
+name="$2"
+echo "pm path \"$name\" | grep -vo -e \"^package:\" | xargs -n 1 unzip -l | grep -e '\.so$' | awk -F/ '{print \$NF}'" | adb shell | sort | uniq | grep -vP $(jq -r .[].signature $signatures | sed -z "s/\n/|/g" | sed -e "s/|$//g")
+}
+
+
+case $1 in
+"all" ) all_installed_apps $@ ;;
+"apk" ) apk $@ ;;
+"name" ) package_name $@ ;;
+esac
